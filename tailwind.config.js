f5e3e0fe38b0d4b858f6d19e0b9e78de5b0a1a5f
Rattaphon-Ply <rattaphonsiri@gmail.com>
2025-06-02 00:11:10 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -65,14 +65,13 @@ module.exports = {
 				'80%': { transform: 'translateX(4px)' },
 				},
 				'fade-in': {
-				'0%': { opacity: 0, transform: 'translateY(30px)' },
-				'60%': { opacity: 0.6, transform: 'translateY(10px)' },
+				'0%': { opacity: 0, transform: 'translateY(20px)' },
 				'100%': { opacity: 1, transform: 'translateY(0)' },
 				},
 			},
 			animation: {
 				shake: 'shake 0.5s ease-in-out',
-				'fade-in': 'fade-in 1s cubic-bezier(0.25, 0.8, 0.25, 1) forwards',
+				'fade-in': 'fade-in 0.8s ease-out forwards',
 			},
     	}
     },
