a47f0aabcfecf6f51f0e843583aea804d7e52cd0
Rattaphon-Ply <rattaphonsiri@gmail.com>
2025-06-04 17:35:55 +0700

    Create TextMyExp.jsx
    Update theme

---
+++ src/components/home/experience/TextMyExp.jsx
@@ -0,0 +1,13 @@
+const TextMyExp = () => {
+  return (
+    <div className="relative w-fit mx-auto mb-4">
+        <h2 className="text-2xl font-bold text-center relative z-10">
+            My Experiences
+        </h2>
+        <div className="absolute bottom-0 left-0 w-full h-1 overflow-hidden">
+            <div className="w-full h-full bg-gradient-to-r from-transparent via-slate-500 dark:via-white to-transparent animate-slide"></div>
+        </div>
+    </div>
+  )
+}
+export default TextMyExp
