1968d6f763f4f70788f9ccae1f773d93721528ef
Rattaphon-Ply <rattaphonsiri@gmail.com>
2025-06-04 17:35:58 +0700

    Update PictureMe.jsx
    Update theme

--- src/components/home/PictureMe.jsx
+++ src/components/home/PictureMe.jsx
@@ -2,9 +2,13 @@ import Me from '@/images/MePic.png';
 
 const PictureMe = () => {
   return (
-    <div className=''>
-        <img src={Me} className='w-48 object-cover rounded-xl shadow-lg filter brightness transition hover:scale-105 duration-300'  />
-    </div>
+  <div className="relative w-fit">
+  <img
+    src={Me}
+    className="w-48 object-cover rounded-xl shadow-lg transition duration-300 hover:scale-105"
+  />
+  <div className="absolute inset-0 rounded-xl pointer-events-none animate-glow-ring z-[-1]"></div>
+</div>
   )
 }
 export default PictureMe
