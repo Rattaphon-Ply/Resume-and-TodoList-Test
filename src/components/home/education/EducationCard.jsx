7d8586b1ec10fe4ea6e113dee5f023f1cd5b220d
Rattaphon-Ply <rattaphonsiri@gmail.com>
2025-06-30 02:22:30 +0700

    Update EducationCard.jsx

--- src/components/home/education/EducationCard.jsx
+++ src/components/home/education/EducationCard.jsx
@@ -8,9 +8,9 @@ const EducationCard = ({ head, title, description, image, projectUrl }) => {
     <div className="block max-w-sm bg-white rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 overflow-hidden">
       <img src={image} alt={title} className="w-full h-48 object-cover" />
       <div className="p-4 bg-gray-600">
-        <h1 className="text-3xl text-white font-semibold mb-2 w-full break-words whitespace-normal">{head}</h1>
-        <h3 className="text-xl text-white font-semibold mb-2">{title}</h3>
-        <p className="text-white text-sm w-full break-words whitespace-normal">{description}</p>
+        <h1 className="text-xl text-white font-bold mb-2 w-full break-words whitespace-normal">{head}</h1>
+        <h3 className="text-md text-white font-semibold mb-2">{title}</h3>
+        <p className="text-sm text-gray-300  w-full break-words whitespace-normal">{description}</p>
       </div>
     </div>
   );
