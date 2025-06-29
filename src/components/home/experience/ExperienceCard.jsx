26a4962b325cbd66b90d7c4255128d396bcfc1d6
Rattaphon-Ply <rattaphonsiri@gmail.com>
2025-06-30 02:22:32 +0700

    Update ExperienceCard.jsx

--- src/components/home/experience/ExperienceCard.jsx
+++ src/components/home/experience/ExperienceCard.jsx
@@ -5,7 +5,7 @@ const ExperienceCard = ({ head, title, description, image, onMoreClick}) => {
       <img src={image} alt={title} className="w-full h-48 object-cover" />
       <div className="p-4 bg-gray-600">
         <h1 className="text-3xl text-white font-semibold mb-2">{head}</h1>
-        <h3 className="text-xl text-white font-semibold mb-2">{title}</h3>
+        {/* <h3 className="text-xl text-white font-semibold mb-2">{title}</h3> */}
         <p className="text-white text-sm w-ful truncate">{description}</p>
         <button
           className="mt-2 px-4 py-2 bg-[#3498DB] text-white rounded hover:bg-blue-600"
