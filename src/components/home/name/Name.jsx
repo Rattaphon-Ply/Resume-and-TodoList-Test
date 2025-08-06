98c6089208d3f604631909a2962054411084e8b2
Rattaphon-Ply <rattaphonsiri@gmail.com>
2025-08-07 05:43:31 +0700

    Create Name.jsx

---
+++ src/components/home/name/Name.jsx
@@ -0,0 +1,16 @@
+import TextType from "@/components/text-animate/TextType" 
+
+const Name = () => {
+  return (
+    <TextType
+        className="text-4xl font-bold mt-4 text-blue-700"
+        text={["Rattaphon Siriphan","รัตพล ศิริพันธ์"]}
+        typingSpeed={150}
+        pauseDuration={1500}
+        showCursor={true}
+        cursorCharacter="_"
+        textColors={"#6600FF"}
+    />
+  )
+}
+export default Name
