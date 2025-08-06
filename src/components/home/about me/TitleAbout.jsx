fbbaeec8850dbc4cb1bb300bff25f6d2a0b04ab0
Rattaphon-Ply <rattaphonsiri@gmail.com>
2025-08-07 05:43:29 +0700

    Create TitleAbout.jsx

---
+++ src/components/home/about me/TitleAbout.jsx
@@ -0,0 +1,37 @@
+import VariableProximity from "@/components/text-animate/VariableProximity";
+import { useRef } from 'react';
+
+
+const TitleAbout = () => {
+    const containerRef = useRef(null);
+
+  return (
+    <div className="w-full">
+      <div className="relative pr-6 max-w-3xl space-y-4 lg:pr-10">
+        {/* เส้นสีด้านซ้าย */}
+        <div className="absolute left-0 top-0 h-full w-1 bg-primary rounded-full" />
+
+        <div className="pl-6 pr-6 space-y-4">
+
+            <div
+                ref={containerRef}
+                style={{position: 'relative'}}
+            >
+                <VariableProximity
+                    label={'สวัสดีครับ ผมเป็นนักศึกษาจบใหม่ เกรดเฉลี่ย 3.57 กำลังมองหางานประจำในตำแหน่งนักพัฒนา (Developer) โดยมีเป้าหมายระยะยาวในการพัฒนาตนเองสู่การเป็น Full-Stack Developer ผมเป็นคนมีความรับผิดชอบ อดทน สามารถทำงานภายใต้ความกดดันได้ดี และพร้อมเปิดรับการเรียนรู้สิ่งใหม่ ๆ อยู่เสมอ'}
+                    className={'variable-proximity-demo leading-relaxed text-base lg:text-lg text-muted-foreground pl-8'}
+                    fromFontVariationSettings="'wght' 300"
+                    toFontVariationSettings="'wght' 900"
+                    containerRef={containerRef}
+                    radius={100}
+                    falloff='linear'
+                />
+            </div>
+        </div>
+      </div>
+
+    </div>
+    
+  );
+};
+export default TitleAbout;
